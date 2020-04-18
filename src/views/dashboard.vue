1951773d15dfbf2224d98564ccd3f64e4319d8c6
crocodilered <serge.zolotukhin@gmail.com>
2020-04-18 17:50:56 +0300

    Tune up dashboard for anons & new users.

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,15 +1,25 @@
 <template>
   <b-container>
     
-    <template v-if="user && kinds">
+    <template v-if="user && vehicles && vehicles.length > 0 && kinds">
       <b-row v-for="(o, k) in vehicles" :key="`vehicle-${k}`" align-v="center" no-gutters class="vehicle">
         <b-col cols="auto"><b-avatar variant="outline-primary" style="border: 2px solid var(--primary);" :src="vehiclePhoto(o)" size="4rem"></b-avatar></b-col>
         <b-col class="message"><b-link :to="vehicleRetrieveHref(o)">{{ vehicleTitle(o) }}</b-link></b-col>
       </b-row>
     </template>
 
+    <template v-if="user && vehicles && vehicles.length === 0">
+      <h1>Для начала</h1>
+      <p class="message">
+        Необходимо <b-link :to="vehicleCreateHref()">добавить автомобиль</b-link>.
+      </p>
+    </template>
+
     <template v-if="!user">
-      anon
+      <h1 style="font-size:2.7rem;">Учет<br> расходов<br> <nobr>на машину</nobr></h1>
+      <p style="font-size:120%;">
+        История обслуживании, ремонта и тюнинга вашего автомобиля.
+      </p>
     </template>
 
   </b-container>
@@ -18,7 +28,7 @@
 <script>
   import { mapState } from 'vuex'
   import { LOAD_KINDS } from '@/store/action-types'
-  import { vehicleTitle, vehiclePhoto, vehicleRetrieveHref } from '@/helpers/vehicle' 
+  import { vehicleTitle, vehiclePhoto, vehicleRetrieveHref, vehicleCreateHref } from '@/helpers/vehicle' 
   
   export default {
     name: 'an-dashboard',
@@ -27,6 +37,7 @@
       vehicleTitle,
       vehiclePhoto,
       vehicleRetrieveHref,
+      vehicleCreateHref,
     },
 
     computed: {
