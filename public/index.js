f1ef2a5e767837d9fd572c380cb01a803c4f8dfa
crocodilered <serge.zolotukhin@gmail.com>
2020-04-19 18:56:00 +0300

    Improve a2hs

--- public/index.js
+++ public/index.js
@@ -1,11 +1,13 @@
 
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && window.location.protocol === 'https:') {
   navigator.serviceWorker.register('/service-worker.js')
 }
 
 
 let deferredPrompt;
 const a2hsPanel = document.getElementById('a2hs');
+const a2hsOk = document.getElementById('a2hs-ok');
+const a2hsCancel = document.getElementById('a2hs-cancel');
 
 
 window.addEventListener('beforeinstallprompt', (e) => {
@@ -14,13 +16,12 @@ window.addEventListener('beforeinstallprompt', (e) => {
   deferredPrompt = e;
   a2hsPanel.style.display = 'block';
 
-  a2hsPanel.addEventListener('click', (e) => {
-    console.log(3)
+  a2hsOk.addEventListener('click', () => {
 
     a2hsPanel.style.display = 'none';
     deferredPrompt.prompt();
-    
-    deferredPrompt.userChoice.then((choiceResult) => {
+
+    deferredPrompt.userChoice.then(choiceResult => {
         if (choiceResult.outcome === 'accepted') {
           console.log('User accepted the A2HS prompt');
         } else {
@@ -28,6 +29,10 @@ window.addEventListener('beforeinstallprompt', (e) => {
         }
         deferredPrompt = null;
       });
+  });
+
+  a2hsCancel.addEventListener('click', () => {
+    a2hsPanel.style.display = 'none';
   });
 
 });
