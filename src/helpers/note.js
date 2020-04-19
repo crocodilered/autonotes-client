939efce98eb51e1906e16bf03232f30f0f1643aa
crocodilered <serge.zolotukhin@gmail.com>
2020-04-19 15:15:24 +0300

    Decode URI filename to display it to user.

--- src/helpers/note.js
+++ src/helpers/note.js
@@ -2,8 +2,18 @@
  * Get filename from file URL
  */
 export function getFilename (url) {
+
+  let decodedUrl
+
   if (url) {
-    const arr = url.split('/')
+    try {
+      decodedUrl = decodeURI(url)
+    } catch(e) { // catches a malformed URI
+      decodedUrl = url
+    }
+
+    const arr = decodedUrl.split('/')
     return arr[arr.length - 1]
+    
   }
 }
